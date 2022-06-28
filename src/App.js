ef88024ee543c40fa2ea7a8c634cedad43786f62
npxrus <npxrus@yandex.com>
2022-06-28 21:52:33 +0300

    feat: add ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'tachyons';
 import Navigation from './components/navigation/navigation.component';
 import Logo from './components/logo/logo.component';
+import ImageLinkForm from './components/image-link-form/image-link-form.component';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Navigation />
       <Logo />
+      <ImageLinkForm />
     </div>
   );
 }
