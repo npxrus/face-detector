6780b27d9f23821c22cf98695f2e871c8758126d
npxrus <npxrus@yandex.com>
2022-06-28 21:32:25 +0300

    feat: add Logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'tachyons';
 import Navigation from './components/navigation/navigation.component';
+import Logo from './components/logo/logo.component';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
+      <Logo />
     </div>
   );
 }
